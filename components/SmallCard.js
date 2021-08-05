ae9cf7eaa0589606f05ef5951b61628f2c8e6714
Sahinur Islam <infosahinur@gmail.com>
2021-08-05 17:00:21 +0600

    small card add

---
+++ components/SmallCard.js
@@ -0,0 +1,9 @@
+function SmallCard({ img, location, distance }) {
+    return (
+        <div>
+            <h1>{img}</h1>
+        </div>
+    )
+}
+
+export default SmallCard
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard'
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -12,6 +13,34 @@ export default function Home() {
 
       <Header/>
       <Banner/>
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endPoints */}
+          {exploreData?.map(({img, distance, location}) => (
+            <SmallCard 
+              key={img}
+              img={img}
+              distance={distance}
+              location={location}  
+            />
+          ))}
+        </section>
+      </main>
     </div>
   )
+}
+
+// api call
+export async function getStaticProps(){
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+  .then(res => res.json())
+
+  return{
+    props:{
+      exploreData
+    }
+  }
 }
