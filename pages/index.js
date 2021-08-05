a4f61318eafc3103dc24f66280e2364aba7d870c
Sahinur Islam <infosahinur@gmail.com>
2021-08-05 18:07:05 +0600

    Medium Card done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 
 export default function Home({ exploreData, cardsData }) {
@@ -33,10 +34,18 @@ export default function Home({ exploreData, cardsData }) {
 
         <section>
           <h2 className='text-4xl font-semibold py-8'>Live Anywehre</h2>
-          {cardsData?.map((item) => (
-            
-          ))}
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData?.map(({img, title}) => (
+              <MediumCard
+                key={img}
+                img={img}
+                title={title}
+              />
+            ))}
+          </div>
         </section>
+
+        
       </main>
     </div>
   )
