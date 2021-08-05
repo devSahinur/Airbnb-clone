5c5ec16e54d0fd6d1416486f66a6534b580176d7
Sahinur Islam <infosahinur@gmail.com>
2021-08-05 15:53:32 +0600

    header done

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { SearchIcon } from '@heroicons/react/solid'
+import { GlobeAltIcon, SearchIcon, MenuIcon, UserCircleIcon } from '@heroicons/react/solid'
 
 function Header() {
     return (
@@ -21,7 +21,15 @@ function Header() {
             </div>
 
             {/* right */}
-            <div></div>
+            <div className='flex items-center space-x-4 justify-end text-gray-500 '>
+                <p className='hidden md:inline cursor-pointer'>Become a host</p>
+                <GlobeAltIcon className='h-6 '/>
+
+                <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                    <MenuIcon className='h-6 '/>
+                    <UserCircleIcon className='h-6 '/>
+                </div>
+            </div>
         </header>
     )
 }
