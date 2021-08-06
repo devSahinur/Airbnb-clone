a62fb6b2e1807b00ae3448ff01393bd6054599e9
Sahinur Islam <infosahinur@gmail.com>
2021-08-06 18:48:13 +0600

    new

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray'>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-200'>
             <div className='space-y-4 text-xs text-gray-800'>
                 <h5 className='font-bold'>ABOUT</h5>
                 <p>How Airbnb works</p>
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,24 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
 function Search() {
     return (
         <div>
-            <h1>hi i am Search page</h1>
+           <Header/>
+
+           <main className='flex'>
+                <section>
+                    <p className='text-xs '>300+ Stay for 5 number of guests</p>
+
+                    <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Sahnur</h1>
+
+                    <div>
+                        <p>Cancellation Flexibility</p>
+                    </div>
+                </section>
+           </main>
+
+           <Footer/>
         </div>
     )
 }
