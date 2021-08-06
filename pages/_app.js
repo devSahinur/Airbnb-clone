9df24111fe98a12d0b751fd8b27ec2533761110f
Sahinur Islam <infosahinur@gmail.com>
2021-08-07 02:12:57 +0600

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import Router from 'next/router'
+
+//progressBar lib
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
